5c82cbee998ede01e27b0ee8e683e78d6a88c170
町田和輝 <kazmat.hui@gmail.com>
2023-01-04 08:30:43 +0900

    compile

--- app.js
+++ app.js
@@ -1,21 +1,13 @@
-function combine(input1, input2, resultConversion) {
-    var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
-        result = +input1 + +input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
-    // if (resultConversion === 'as-number') {
-    //   return +result;
-    // } else {
-    //   return result.toString();
-    // }
+"use strict";
+let userInput;
+let userName;
+userInput = 5;
+userInput = 'Max';
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
-var combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges);
-var combinedStringAges = combine('30', '26', 'as-number');
-console.log(combinedStringAges);
-var combinedNames = combine('Max', 'Anna', 'as-text');
-console.log(combinedNames);
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+    // while (true) {}
+}
+generateError('An error occurred!', 500);
--- objs-arrays-enums.js
+++ objs-arrays-enums.js
@@ -1,3 +1,4 @@
+"use strict";
 // const person: {
 //   name: string;
 //   age: number;
@@ -22,21 +23,19 @@ var Role;
     Role[Role["READ_ONLY"] = 100] = "READ_ONLY";
     Role["AUTHOR"] = "AUTHOR";
 })(Role || (Role = {}));
-;
-var person = {
+const person = {
     name: 'Maximilian',
     age: 30,
     hobbies: ['Sports', 'Cooking'],
-    role: Role.ADMIN
+    role: Role.ADMIN,
 };
 // person.role.push('admin');
 // person.role[1] = 10;
 // person.role = [0, 'admin', 'user'];
-var favoriteActivities;
+let favoriteActivities;
 favoriteActivities = ['Sports'];
 console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
+for (const hobby of person.hobbies) {
     console.log(hobby.toUpperCase());
     // console.log(hobby.map()); // !!! ERROR !!!
 }
